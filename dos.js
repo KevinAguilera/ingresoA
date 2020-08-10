1085388eccab9256290fed13f8bddcb7cbcbab69
KevinAguilera <67611246+KevinAguilera@users.noreply.github.com>
2020-08-10 10:06:48 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -7,5 +7,86 @@ b)la marca del más caro de los líquidos
 c)la marca del más liviano de los sólidos
 */function mostrar()
 {
-  alert("dos");
+  let marca;
+  let precio;
+  let kilos;
+  let tipo;
+  let acumKilos = 0;
+  let contadorLiquido = 0;
+  let contadorSolido = 0;
+  let liquidoCaro;
+  let marcaCara;
+  let solidoLiviano;
+  let marcaSolidoLiviano;
+  let seguir;
+  let flagLiquidoCaro = 0;
+  let flagSolidoLiviano = 0;
+
+
+  do{
+
+    marca = prompt("Ingrese una marca: ");
+    while(!(isNaN(marca))){
+         marca = prompt("Marca inválida. Ingrese una marca");
+    }
+    
+    precio = parseFloat(prompt("Ingrese precio: "));
+	while(precio <= 0 || isNaN(precio)){
+		precio = parseFloat(prompt("Precio inválido. Ingrese preci: "));
+	}
+	
+    kilos = parseInt(prompt("Ingrese kilos: "));
+    while(kilos <= 0 || isNaN(kilos)){
+        kilos = parseInt(prompt("Error. Ingrese kilos: "));
+    }
+
+    tipo = prompt("Ingrese tipo (solido o liquido)");
+    while(tipo != "solido" && tipo != "liquido"){
+        tipo = prompt("Tipo invalido. Ingrese tipo solido o liquido");
+    }
+
+    acumKilos += kilos;
+
+    switch(tipo){
+        case "liquido":
+            contadorLiquido++;
+
+            if(flagLiquidoCaro == 0 || precio > liquidoCaro){
+                liquidoCaro = precio;
+                marcaCara = marca;
+                flagLiquidoCaro = 1;
+            }
+
+            break;
+        case "solido":
+            contadorSolido++;
+
+            if(flagSolidoLiviano == 0 || kilos < solidoLiviano){
+                solidoLiviano = kilos;
+                marcaSolidoLiviano = marca;
+                flagSolidoLiviano = 1;
+            }
+            
+            break;
+    }
+
+    seguir = prompt("Quiere ingresar otra marca?");
+  }while(seguir == "s");
+  
+
+    document.write(" A) Peso total de la compra: " + acumKilos + "</br>");
+
+    if(flagLiquidoCaro == 0){
+        document.write( "B) No hay liquidos" + "</br>");
+
+    }else{
+        document.write(" B) Marca del más caro de los líquidos: " + marcaCara + "</br>");
+    }
+
+    if(flagSolidoLiviano == 0){
+        document.write( "C) No hay sólidos");
+    
+    }else{
+        document.write(" C) Marca del más liviano de los sólidos: " + marcaSolidoLiviano + "</br>");
+    }
 }
--- uno.js
+++ uno.js
@@ -9,5 +9,70 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	let nombre;
+    let temperatura;
+    let sexo;
+    let edad;
+    let mayorTemperatura;
+    let nombreMayorTemperatura;
+    let flagMayorTemperatura = 0;
+    let contadorHombre = 0;
+    let contadorMujer = 0;
+    let acumPersonas = 0;
+    let promedioEdad;
+
+
+    for(let i = 0;i < 5; i++){
+
+        nombre = prompt("Ingrese su nombre");
+        while(!(isNaN(nombre))){
+            nombre = prompt("Nombre inválido. Ingrese su nombre");
+        }
+    
+        temperatura = parseInt(prompt("Ingrese su temperatura corporal (entre 35 y 50)"));
+        while(!(temperatura >= 35 && temperatura <= 50 )){
+            temperatura = parseInt(prompt("Temperatura inválida. Ingrese temperatura mínima(entre 35 y 50)"));
+        }
+
+       sexo = prompt("Ingrese su sexo(f o m)").toLowerCase();
+        while(!(sexo == 'f' || sexo == 'm')){
+            sexo = prompt('sexo inválido. Ingrese f o m').toLowerCase();
+        }
+
+        edad = parseInt(prompt("Ingrese su edad: "));
+        while(!(edad > 0 && edad < 150)){
+            edad = parseInt(prompt("Edad invalida. Ingrese su edad"));
+        }
+        
+        acumPersonas+= edad;
+
+
+        switch(sexo){
+            case "f":
+                contadorMujer++;
+                
+                if(flagMayorTemperatura == 0 || temperatura > mayorTemperatura){
+
+                    mayorTemperatura = temperatura;
+                    nombreMayorTemperatura = nombre;
+                    flagMayorTemperatura = 1;
+                }
+                break;
+            case "m":
+                contadorHombre++;
+
+                break;
+        }
+    }
+
+        promedioEdad = acumPersonas / 5;
+
+        document.write(" A) Cantidad de mujeres: " + contadorMujer + " Cantidad de hombres: "+ contadorHombre +"</br>");
+        document.write(" B) Edad promedio total: " + promedioEdad);
+
+        if(flagMayorTemperatura == 0){
+            document.write( "No hay mujeres" + "</br>");
+        }else{
+            document.write(" C) Mujer con más temperatura: " + nombreMayorTemperatura + "</br>");
+        }
 }
